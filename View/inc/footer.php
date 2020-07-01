35c58dbfd0e3e8252d307e0ef97dd794749d597e
pacojuanes <61423016+pacojuanes@users.noreply.github.com>
2020-07-01 14:06:52 +0200

    Initial commit

---
+++ View/inc/footer.php
@@ -0,0 +1,17 @@
+
+
+
+<footer class="#76ff03 light-green accent-3">
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="row footer-row">
+        <div class="col l6 s12">
+          Copyright © <?= date("Y"); ?> ouached atman
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <a href="<?=ROOT_URL?>blog_legalNotice.html" class="white-text">Mentions légales</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
